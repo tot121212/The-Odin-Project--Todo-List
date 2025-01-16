0d765d4a8c800c12fdd6949735794316a41eba09
tot121212 <totmusicbusiness@gmail.com>
2025-01-16 01:47:11 -0600

    HTMLHandler partially done

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ class Project {
         this.label = label;
         this.todoLists = [];
     }
-    
+
     addTodoList(todoList, idx) {
         if (idx === undefined) {
             this.todoLists.push(todoList);
@@ -63,8 +63,10 @@ class Project {
 }
 
 class Projects {
+    // it is not necessary to load all projects at once but i already started doing it this way so eh, ideally i would get the currentProject from local storage
     constructor() {
         this.projects = [];
+        this.currentProject = null;
         this.addDefaultProject();
     }
 
@@ -94,6 +96,7 @@ class Projects {
         project.addTodoList(new TodoList("Default"));
         project.getTodoLists()[0].addTodo(new Todo("Todo", "Description", "Due Date", "Priority"));
         this.addProject(project);
+        this.currentProject = this.getProjects()[0];
     }
 }
 
@@ -101,7 +104,93 @@ class HTMLHandler {
     constructor() {
     }
 
-    initialize
+    // need to be able to move elements around, delete them, edit them, all whilst said elements are saved correctly...
+    // easiest way would be give each dom element a uuid and just find that specific element via data-id but thats annoying
+
+    static addLabelToElement = (element)=>{
+        const labelElement = document.createElement("div");
+        labelElement.classList.add("label");
+        element.appendChild(labelElement);
+    }
+
+
+    static createTodoElement = (todo)=>{
+        const todoElement = document.createElement("div");
+        todoElement.classList.add("todo");
+        return todoElement;
+    }
+
+    static createTodosContainer = ()=>{
+        const todosContainer = document.getElementById("todos");
+        todosContainer.classList.add("todos");
+        return todosContainer;
+    }
+
+    static createTodos = (todos) => {
+        const todosContainer = this.createTodosContainer();
+        todos.forEach(todo => {
+            const todoElement = createTodoElement(todo);
+            todosContainer.appendChild(todoElement);
+        });
+    }
+
+
+    // need to be able to move todo lists around so move in array save to storage, never worry abt updating page at all cuz we working with either new elements or existing ones.
+    static updateTodoLists = (todoLists, projectElement) => {
+        const todoListsContainer = projectElement.querySelector("div.todo-lists");
+        todoListsContainer.innerHTML = "";
+        todoLists.forEach(todoList => {
+            const todoListElement = this.createTodoListElement(todoList);
+            todoListsContainer.appendChild(todoListElement);
+        });
+    }
+
+    static createTodoListElement = (todoList) => {
+        const todoListElement = document.createElement("div");
+        todoListElement.classList.add("todo-list");
+        this.addLabelToElement(todoListElement);
+        return todoListElement;
+    }
+
+    static createTodoListsContainer = ()=>{
+        const todoListsContainer = document.createElement("div");
+        todoListsContainer.classList.add("todo-lists");
+        return todoListsContainer;
+    }
+
+    static createTodoLists = (todoLists, projectElement) => {
+        const todoListsContainer = this.createTodoListsContainer();
+        
+        todoLists.forEach(todoList => {
+            const todoListElement = this.createTodoListElement(todoList);
+            todoListsContainer.appendChild(todoListElement);
+        });
+
+        projectElement.appendChild(todoListsContainer);
+    }
+
+
+    static createProjectElement = (project)=>{
+        const projectElement = document.createElement("div");
+        projectElement.classList.add("project");
+
+        this.addLabelToElement(projectElement);
+        return projectElement;
+    }
+
+    static createProject = (project) => {
+        const projectElement = this.createProjectElement(project);
+
+        this.createTodoLists(project.getTodoLists(), projectElement);
+        this.updateTodoLists(project.getTodoLists(), projectElement);
+
+        document.body.appendChild(projectElement);
+    }
+
+    static destroyExistingProject = () => {
+        const existingProject = document.querySelector("div.project");
+        if (existingProject) { existingProject.remove(); }
+    }
 }
 
 class LocalStorageHandler {
@@ -109,7 +198,7 @@ class LocalStorageHandler {
     }
 
     // gets projects object from local or makes new
-    getProjects = ()=>{
+    static getProjects = ()=>{
         let projects = localStorage.getItem("projects");
         if (projects) {
             return JSON.parse(projects);
@@ -122,8 +211,9 @@ class LocalStorageHandler {
 
 document.addEventListener("DOMContentLoaded", () => {
     const htmlHandler = new HTMLHandler();
-    const localStorageHandler = new LocalStorageHandler();
-    const projects = localStorageHandler.getProjects();
+    const projects = LocalStorageHandler.getProjects();
     console.log("Projects: ");
     console.log(projects);
+    HTMLHandler.destroyExistingProject();
+    HTMLHandler.createProject(projects.currentProject);
 });

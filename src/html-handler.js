0d566d1b0f5f849b0b542a5490c4d53e83b0eb08
tot121212 <totmusicbusiness@gmail.com>
2025-01-24 15:20:16 -0600

    added loadProjectsList back to html-handler

--- src/html-handler.js
+++ src/html-handler.js
@@ -111,4 +111,37 @@ export class HTMLHandler {
         const existingProject = document.querySelector("div.project");
         if (existingProject) { existingProject.remove(); }
     }
+
+    static loadProjectsList = ()=>{
+        this.clearContent();
+    
+        const projectContainer = document.createElement("div");
+        projectContainer.classList.add("projects-container");
+        document.querySelector(".content").append(projectContainer);
+    
+        const projectList = Projects.list;
+        for (const project of projectList){
+            const projectElement = this.createProjectElement(project);
+            projectContainer.appendChild(projectElement);
+        }
+    
+        projectContainer.addEventListener("click", (e)=>{
+            if (e.target.classList.contains("project")){
+                const projectNameElement = e.target.querySelector(".project-name");
+                if (!projectNameElement) {
+                    throw new Error("Project name element not found");
+                }
+                for (const project of Projects.list){
+                    if (projectNameElement.textContent === project.name){
+                        this.clearContent();
+                        const projectElement = this.createProjectElement(project);
+                        const todoListsElement = this.createTodoListsElement(project.todoLists, projectElement);
+                        projectElement.append(todoListsElement);
+                        this.initializeProjectElement(project, projectElement);
+                        break;
+                    }
+                }
+            }
+        });
+    }
 }
